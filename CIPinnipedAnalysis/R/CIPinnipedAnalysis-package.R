7127a8a8e21760d3011f3e2790060521a3fb7fac
Jeff Laake <jefflaake@gmail.com>
2014-11-13 16:06:10 -0800

    added popan.results documentation

--- CIPinnipedAnalysis/R/CIPinnipedAnalysis-package.R
+++ CIPinnipedAnalysis/R/CIPinnipedAnalysis-package.R
@@ -1,5 +1,5 @@
-
-
+#' CIPinnipedAnalysis
+#' 
 #' Channel Islands Pinniped Analysis functions
 #' : pup production, early pup mortality, SST and other enviromental measurement computations
 #' 
@@ -21,7 +21,24 @@
 #' prod=do.pup.production()
 #' do.early.pup.mortality()
 #' 
+
 NULL
+
+#' POPAN model results and correction factors from dead pup tagging data
+#' 
+#' POPAN results and correction factor data (estimates) that are used to correct dead
+#' pup counts for years without tagging data.
+#' 
+#' @name smi1994.popan.results
+#' @aliases smi1994.popan.results smi1995.popan.results smi1998.popan.results smi2002.popan.results sni2006.popan.results
+#' @docType data
+#' @format A list with elements: popan.results - a marklist of fitted POPAN models, cfdata - list of correction factor data used to correct
+#' dead pup counts for years without tagging data, and cfbyocc - dataframe of correction factors for each occasion. 
+#' @keywords datasets
+
+NULL
+
+
 
 
 
